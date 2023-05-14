d8e941fed22d77aed2cea28ea794047beb86ec38
wiggapony0925 <ninjeff06@gmail.com>
2023-05-14 16:01:03 -0400

    imported templates and chains

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import os
 from apikey import APIKEY
 import streamlit as st 
 from langchain.llms import OpenAI
-
-
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 os.environ['OPENAI_API_KEY'] = APIKEY
 
 #app
 st.title("Youtube Gpt")
 prompt = st.text_input("plug in your prompt")
+
+#templates
 
 #llms 
 llm = OpenAI(temperature=0.9) #creativity
