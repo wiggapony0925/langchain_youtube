d2961010dfe905c82a0c1e805bf6286fdc3be70e
wiggapony0925 <ninjeff06@gmail.com>
2023-05-14 23:01:54 -0400

    title -+> scripts

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from apikey import APIKEY
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SimpleSequentialChain
 
 os.environ['OPENAI_API_KEY'] = APIKEY
 
@@ -13,13 +13,14 @@ st.title("Youtube Gpt")
 prompt = st.text_input("plug in your prompt")
 
 #templates
-title_template = PromptTemplate(
-    input_variables= ['topic'],
-    template = 'write me a youtube video tittle about {topic}'
+script_template = PromptTemplate(
+    input_variables= ['title'],
+    template = 'write me a youtube video script based on this title:  {topic}'
 )
+# generate the script
 #llms 
 llm = OpenAI(temperature=0.9) #creativity
-title_chain = LLMChain(llm=llm, prompt= title_template, verbose=True)
+title_chain = LLMChain(llm=llm, prompt= script_template, verbose=True)
 
 #screen output
 if prompt:
