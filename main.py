20a8caec8b028e558f3e620312f46249eb772ff6
wiggapony0925 <ninjeff06@gmail.com>
2023-05-17 19:03:27 -0400

    imported the wikipidead api

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 os.environ['OPENAI_API_KEY'] = APIKEY
 
@@ -23,10 +25,13 @@ script_template = PromptTemplate(
     template = 'write me a youtube video script based on this title:  {topic}'                  
 )
 # generate the script
+
+#Memeroy
+memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
 #llms 
 llm = OpenAI(temperature=0.9) #creativity
-title_chain = LLMChain(llm=llm, prompt= title_template, verbose=True, output_key='title')
-script_chain = LLMChain(llm=llm, prompt= script_template, verbose=True, output_key='script')
+title_chain = LLMChain(llm=llm, prompt= title_template, verbose=True, output_key='title', memory=memory)
+script_chain = LLMChain(llm=llm, prompt= script_template, verbose=True, output_key='script', memory=memory)
 
 #chain these two isntances together
 sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title', 'script'], verbose=True)
@@ -36,3 +41,7 @@ if prompt:
     response = sequential_chain({'topic': prompt})
     st.write(response['title'])
     st.write(response['scripts'])
+    
+    #memory output
+    with st.expander('message history'):
+        st.info(memory.buffer)
