b191373d58bafbe3b0fb13b090cf59050bc58c4b
wiggapony0925 <ninjeff06@gmail.com>
2023-05-17 18:55:53 -0400

    simplesc to sc

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from apikey import APIKEY
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
 
 os.environ['OPENAI_API_KEY'] = APIKEY
 
@@ -20,18 +20,19 @@ title_template = PromptTemplate(
 
 script_template = PromptTemplate(
     input_variables= ['title'],
-    template = 'write me a youtube video script based on this title:  {topic}'
+    template = 'write me a youtube video script based on this title:  {topic}'                  
 )
 # generate the script
 #llms 
 llm = OpenAI(temperature=0.9) #creativity
-title_chain = LLMChain(llm=llm, prompt= title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt= script_template, verbose=True)
+title_chain = LLMChain(llm=llm, prompt= title_template, verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt= script_template, verbose=True, output_key='script')
 
 #chain these two isntances together
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)#list of sequence 
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'], output_variables=['title,', 'script '], verbose=True)#list of sequence 
 
 #screen output
 if prompt:
-    response = sequential_chain.run(prompt)
-    st.write(response)
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['scripts'])
